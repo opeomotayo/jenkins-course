10aa8162b7b1a3102602dc419395b0b13e85dfd4
Ope Omotayo <opeomotayo@gmail.com>
2021-08-31 17:51:41 +0100

    update job-dsl folder

--- job-dsl/tryPipelineWithBuildPod.groovy
+++ job-dsl/tryPipelineWithBuildPod.groovy
@@ -3,22 +3,21 @@ pipelineJob('pipeline-job-with-build-pod') {
         cps {
             script('''
         pipeline {
-              agent {
-                  kubernetes {
-                    defaultContainer 'docker'
-                    yamlFile 'BuildPod.yaml'
-                  }
-              }
-                stages {
-                    stage('Stage 1') {
-                        steps {
-                            echo 'logic'
-                        }
+            agent {
+                kubernetes {
+                defaultContainer 'docker'
+                yamlFile 'BuildPod.yaml'
+                }
+            }
+            stages {
+                stage('Stage 1') {
+                    steps {
+                        echo 'logic'
                     }
-                    stage('Stage 2') {
-                        steps {
-                            echo 'logic'
-                        }
+                }
+                stage('Stage 2') {
+                    steps {
+                        echo 'logic'
                     }
                 }
             }
