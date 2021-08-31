aa2faf5492644d12f65dee7e57b829c54f41052a
Ope Omotayo <opeomotayo@gmail.com>
2021-08-31 18:16:29 +0100

    update job-dsl folder

--- job-dsl/tryPipelineWithBuildPod.groovy
+++ job-dsl/tryPipelineWithBuildPod.groovy
@@ -1,42 +1,33 @@
-pipelineJob('pipeline-job-with') {
-    definition {
-        cps {
-//            script(readFileFromWorkspace('project-a-workflow.groovy'))
-            sandbox()
+pipelineJob('pipeline-job-with-buildpod') {
+    description('pipeline-job-with-buildpod')
+    logRotator {
+        numToKeep(10)
+        daysToKeep(5)
+    }
+    properties {
+        gitLabConnection {
+            gitLabConnection('gitlab-creds')
         }
     }
+    parameters {
+        stringParam('BRANCH', 'master', 'Branch')
+    }
+
+    definitions {
+        cpsScm {
+            git {
+                extension {
+                    wipeOutWorkspace()
+                }
+                remote {
+                    url('ssh://git@gitlab.com:opeomotayo/helloworld-app.git')
+                    credentials('jenkins')
+                }
+                branch("$BRANCH")
+            }
+        }
+        scriptPath('Jenkinsfile')
+    }
+
 }
-//pipelineJob('pipeline-job-with-build-pod') {
-//    definition {
-//        description('')
-//        logRotator {
-//            numToKeep(10)
-//            daysToKeep(5)
-//        }
-//        properties {
-//            gitLabConnection {
-//                gitLabConnection('gitlab-creds')
-//            }
-//        }
-//        parameters {
-//            stringParam('BRANCH', 'master', 'Branch')
-//        }
-//
-//        definitions {
-//            cpsScm {
-//                git {
-//                    extension {
-//                        wipeOutWorkspace()
-//                    }
-//                    remote {
-//                        url('ssh://git@gitlab.com:opeomotayo/helloworld-app.git')
-//                        credentials('jenkins')
-//                    }
-//                    branch("$BRANCH")
-//                }
-//            }
-//            scriptPath('Jenkinsfile')
-//        }
-//    }
-//}
-//
+
