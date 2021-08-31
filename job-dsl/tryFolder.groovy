0f157d9d9c46f0704caff880d2c2277a1eabd24c
Ope Omotayo <opeomotayo@gmail.com>
2021-08-31 13:39:09 +0100

    update job-dsl folder

--- job-dsl/tryFolder.groovy
+++ job-dsl/tryFolder.groovy
@@ -1,8 +1,8 @@
-folder('folder-a') {
-    displayName('Folder A')
+folder('project-a') {
+    displayName('Project A')
     description('Folder for project A')
 }
 
-folder('folder-a/testing') {
+folder('project-a/testing') {
     description('Folder containing all QA jobs for project A')
 }
